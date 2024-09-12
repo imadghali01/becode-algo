27066ccf364724d5a34792649499e3eecb1e7116
imadghali01 <imadghali01@gmail.com>
2024-09-12 12:52:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,23 @@
-/*Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. 
-For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10.
-Notes
+/*A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
 
-    Expect any positive number between 1 and 1000.
-
-Expected results
-
-addUp(4) ➞ 10
-
-addUp(13) ➞ 91
-
-addUp(600) ➞ 180300
-|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
-
-Create a function that takes an array of numbers and return both the minimum and maximum numbers, in that order.
-Notes
-
-    All test arrays will have at least one element and are valid.
+    The wanted word is in lowercase.
+    The crowd of letters is all in uppercase.
+    Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
 
 Expected results
 
-minMax([1, 2, 3, 4, 5]) ➞ [1, 5]
+detectWord("UcUNFYGaFYFYGtNUH") ➞ "cat"
 
-minMax([2334454, 5]) ➞ [5, 2334454]
+detectWord("bEEFGBuFBRrHgUHlNFYaYr") ➞ "burglar"
 
-minMax([1]) ➞ [1, 1]
-
+detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment") ➞ "embezzlement"
 */
-function sumToTheSpot(aNumber){
-    let totalSum=0;
-    for(i=1 ; i<= aNumber; i++){
-        totalSum = totalSum+i;
-    }
-    return totalSum;
+function detectWord(aString){
+    
+    let stringFilter=aString.replace( /[^a-z]/g, '' );;
+    return stringFilter;
 }
-console.log(sumToTheSpot(4));
-
-function minMaxInArray(anArray){
-    let max=anArray[0];
-    let min=anArray[0];
-    for(i=0 ; i < anArray.length ; i++){
-        if(anArray[i] > max){
-            max = anArray[i];
-        }
-        else if (anArray[i] < min){
-            min = anArray[i];
-        }
-    }
-    let minMaxArray = [min, max];
-    return minMaxArray;
-}
-console.log(minMaxInArray([1, 2, 3, 4, 5]));
-console.log(minMaxInArray([2334454, 5]));
-console.log(minMaxInArray([1]));
+console.log(detectWord("UcUNFYGaFYFYGtNUH"));
+console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
 
