70bf297d548db8e167325179a2fef1e2d4e734aa
imadghali01 <imadghali01@gmail.com>
2024-09-12 15:38:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
-/*Create a function which returns the number of true values there are in an array.
+/*In a board game, a piece may advance 1-6 tiles forward depending on the number rolled on a six-sided die. If you advance your piece onto the same tile 
+as another player's piece, both of you earn a bonus.
+
+Can you reach your friend's tile number in the next roll? Create a function that takes your position a and your friend's position b and 
+returns a boolean representation of whether it's possible to earn a bonus on any die roll.
 Notes
 
-    Return 0 if given an empty array.
-    All array items are of the type bool (true or false).
+    You cannot move backward (which is why example #3 doesn't work).
+    If you are already on the same tile, return false, as you would be advancing away.
+    Expect only positive integer inputs.
 
 Expected results
 
-countTrue([true, false, false, true, false]) ➞ 2
+possibleBonus() ➞ true
 
-countTrue([false, false, false, false]) ➞ 0
+possibleBonus(1, 9) ➞ false
 
-countTrue([]) ➞ 0
+possibleBonus(5, 3) ➞ false
 */
-function trueCounter(array){
-    let trueCount=0;
-    array.forEach(element => {
-        if(element == true){
-            trueCount++;
-        }
-    });
-    return trueCount;
-}
 
-console.log(trueCounter([true, false, false, true, false]));
-console.log(trueCounter([false, false, false, false]));
-console.log(trueCounter([]));
+function possibleBonus(positionA, positionB){
+    if((positionA-positionB) <= 0 && (positionB-positionA) < 7){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+console.log(possibleBonus(3, 7));
+console.log(possibleBonus(1, 9));
+console.log(possibleBonus(5, 3));
