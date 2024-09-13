c8a0afa6d038aa7d3d3e94326ed935f1e86b693b
imadghali01 <imadghali01@gmail.com>
2024-09-13 15:25:18 +0200

    Update app.js

--- app.js
+++ app.js
@@ -26,3 +26,33 @@ sortIt([[4], 1, [3]]) ➞ [1, [3], [4]]
 
 sortIt([[3], 4, [2], [5], 1, 6]) ➞ [1, [2], [3], 4, [5], 6]
 */
+function sortIt(array){
+
+    let sortedArray = [];
+    
+    for(let i=0 ; i<array.length ; i++) {
+        if (Array.isArray(array[i])){
+            if(array[i][0] < sortedArray[i]){
+                sortedArray.unshift(array[i]);
+            }
+            else if(array[i][0] > sortedArray[sortedArray.length-1]){
+                sortedArray.push(array[i]);
+            }
+        }
+        else{
+            if(array[i] < sortedArray[0]){
+                sortedArray.unshift(array[i]);
+            }
+            else if(array[i] > sortedArray[sortedArray.length-1]){
+                sortedArray.push(array[i]);
+            }
+        }
+    }
+
+    return sortedArray;
+}
+console.log(sortIt([4, 1, 3]));
+console.log(sortIt([[4], [1], [3]]));
+console.log(sortIt([4, [1], 3]));
+console.log(sortIt([[4], 1, [3]]));
+console.log(sortIt([[3], 4, [2], [5], 1, 6]));
