b90f4fe3f048a866846f9c4910b87a10f1795f9d
imadghali01 <imadghali01@gmail.com>
2024-09-16 12:19:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,36 @@ calculator(2, "*", 2) ➞ 4
 calculator(4, "/", 2) ➞ 2
 
 */
+function calculator(nbr1, operator, nbr2){
+    switch(operator){
+        case '+' :
+            return nbr1 + nbr2;
+        break;
+
+        case '-' :
+            return nbr1 - nbr2;
+        break;
+
+        case '*' :
+            return nbr1 * nbr2;
+        break;
+
+        case '/' :
+            if(nbr2 == 0){
+                return "Can't divide by 0!";
+            }
+            else{
+                return nbr1 / nbr2;
+            }
+        break;
+        
+        default : 
+            return "this operation is not possible!";
+    }
+
+}
+console.log(calculator(2, "+", 2));
+console.log(calculator(2, "*", 2));
+console.log(calculator(4, "/", 2));
+console.log(calculator(4, "/", 0));
+console.log(calculator(4, "x", 2));
