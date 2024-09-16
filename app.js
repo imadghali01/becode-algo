c3ec8a4d49fcab2544bc38529b7d9c2f83dd94f4
imadghali01 <imadghali01@gmail.com>
2024-09-16 15:13:46 +0200

    algo-p2-exo9

---
+++ app.js
@@ -0,0 +1,25 @@
+/*Given an input string, reverse the string word by word, the first word will be the last, and so on.
+Notes
+
+    A word is defined as a sequence of non-space characters.
+    The input string may contain leading or trailing spaces. However, your reversed string should not contain leading or trailing spaces.
+    You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+Expected results
+
+reverseWords(" the sky is blue") ➞ "blue is sky the"
+
+reverseWords("hello   world!  ") ➞ "world! hello"
+
+reverseWords("a good example") ➞ "example good a"
+*/
+function reverseWords(string){
+    
+    
+    let trimReverser = string.trim().split(' ').reverse();
+    return trimReverser.toString().replace(/,/g, " ");
+}
+
+console.log(reverseWords(" the sky is blue"));
+console.log(reverseWords("hello   world!  "));
+console.log(reverseWords("a good example"));
