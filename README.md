7fbe0d6acb1ae7990ec60299e46ba1a541e634c6
imadghali01 <imadghali01@gmail.com>
2024-09-18 15:11:53 +0200

    algo-part3-exo3

--- README.md
+++ README.md
@@ -1,17 +1,24 @@
 # becode-algo
  algo
  
- ## algo part3 exo 2
+ ## algo part3 exo 3
 
-Given a string, reverse all the words which have odd length. The even length words are not changed.
+Carlos is a huge fan of something he calls smooth sentences.
+
+A smooth sentence is one where the last letter of each word is identical to the first letter the following word (and not case sensitive, so "A" would be the same as "a").
+
+The following would be a smooth sentence "Carlos swam masterfully" because "Carlos" ends with an "s" and swam begins with an "s" and swam ends with an "m" and masterfully begins with an "m".
+
+Create a function that determines whether the input sentence is a smooth sentence, returning a boolean value true if it is, false if it is not.
 Notes
 
-There is exactly one space between each word and no punctuation is used.
+    The last and first letters are case insensitive.
+    There will be no punctuation in each sentence.
+
 Expected results
 
-reverseOdd("Bananas") ➞ "sananaB"
+isSmooth("Marta appreciated deep perpendicular right trapezoids") ➞ true
 
-reverseOdd("One two three four") ➞ "enO owt eerht four"
+isSmooth("Someone is outside the doorway") ➞ false
 
-reverseOdd("Make sure uoy only esrever sdrow of ddo length")
-➞ "Make sure you only reverse words of odd length"
+isSmooth("She eats super righteously") ➞ true
--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-/*Given a string, reverse all the words which have odd length. The even length words are not changed.
+/*Carlos is a huge fan of something he calls smooth sentences.
+
+A smooth sentence is one where the last letter of each word is identical to the first letter the following word (and not case sensitive, so "A" would be the same as "a").
+
+The following would be a smooth sentence "Carlos swam masterfully" because "Carlos" ends with an "s" and swam begins with an "s" and swam ends with an "m" and masterfully begins with an "m".
+
+Create a function that determines whether the input sentence is a smooth sentence, returning a boolean value true if it is, false if it is not.
 Notes
 
-There is exactly one space between each word and no punctuation is used.
+    The last and first letters are case insensitive.
+    There will be no punctuation in each sentence.
+
 Expected results
 
-reverseOdd("Bananas") ➞ "sananaB"
+isSmooth("Marta appreciated deep perpendicular right trapezoids") ➞ true
 
-reverseOdd("One two three four") ➞ "enO owt eerht four"
+isSmooth("Someone is outside the doorway") ➞ false
 
-reverseOdd("Make sure uoy only esrever sdrow of ddo length")
-➞ "Make sure you only reverse words of odd length"
+isSmooth("She eats super righteously") ➞ true
 */
-function reverseOdd(string){
 
-    let stringSpliter = string.trim().split(' ');
-    const wordsInverser = stringSpliter.map(word => word.length % 2 !== 0 ? word.split('').reverse().join('') : word);
-    return wordsInverser.join(' ');
-
+function isSmooth(string){
+    let sToArray = string.split(' ');
+    let tOrF = true;
+    sToArray.forEach(element => {
+        element.split('');
+    });
+    for (let i = 0; i < sToArray.length-2; i++) {
+        if(!(sToArray[i][sToArray[i].length-1] == sToArray[i+1][0])){
+            tOrF = false;
+            break;
+        }
+    }
+    return tOrF;
 }
-console.log(reverseOdd("Bananas"));
-console.log(reverseOdd("One two three four"));
-console.log(reverseOdd("Make sure uoy only esrever sdrow of ddo length"));
+console.log(isSmooth("Marta appreciated deep perpendicular right trapezoids"));
+console.log(isSmooth("Someone is outside the doorway"));
+console.log(isSmooth("She eats super righteously"));
