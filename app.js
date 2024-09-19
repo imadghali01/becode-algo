5f9a3d93255ea077b98e321d5558828e14c68122
imadghali01 <imadghali01@gmail.com>
2024-09-19 16:48:10 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,18 @@
-/*A palindrome is any word or phrase that spells the same way both backward and forward.
+/*Caesar Cipher is a way of "encrypting" a text that, supposably, Julius Caesar used in his communication.
 
-For example : race car is a palindrome. (don't take spaces, periods, dots, etc in consideration)
+We switch every letter with, for example, two letters before in the alphabet.
 
-Other examples : Madam, I'm Adam, radar, kayak, etc.
-
-You should create a function isPalindrome that takes a parameter text and returns a boolean indicating if the provided text is a palindrome or not.
+The Caesar cipher function will take two parameters str and num; str represents the text we want to cipher and num de amount of letters you want to go forward or backward (using negative numbers)
 Notes
 
-    You should be able to to ignore all special characters, spaces, dots, periods, etc. and be case insensitive
+    Make it case incensitive
+    You should be able to loop back or forward into de the alphabet (if you have to go 3 from z, it would be c)
 
 Expected results
 
-isPalindrome("kayak") ---> true
-isPalindrome("race car") ---> true
-isPalindrome("hello world") ---> false
-isPalindrome("Madam, I'm Adam") ---> true*/
+caesarCipher("zoo keeper", 2) ----> "bqq mggrgt"
 
-//effacer les espaces/, et autre signes / comparer en partant du charactere au centre du tableau si la chaine de character a la fin de la string == l inverse d une chaine  au debut
+caesarCipher("bqq mggrgt", -2) ----> "zoo keeper"
 
-function isPalindrome(string){
-    let stringsplit = string.split('');
-    let stringToA = arrayCleaner(stringsplit).filter(function(chaine) {
-        return chaine !== ''; 
-      });;
-    let count = 0;
-    function arrayCleaner(array) {
-        return array.map(function(chaine) {
-          return chaine.replace(/[\W_]+/g, '').toLowerCase()
-        });
-    }
-    for (let i = stringToA.length-1; i > (stringToA.length/2); i--) {
-        if(!(stringToA[i] == stringToA[count])){
-            return false;
-        }
-        count++;
-    }
-    return true;
-}
-console.log(isPalindrome("kayak"));
-console.log(isPalindrome("race car"));
-console.log(isPalindrome("hello world"));
-console.log(isPalindrome("Madam, I'm Adam"));
+caesarCipher("My name is Henrique", 3) ----> "pb qdph lv khqultxh "
+*/
