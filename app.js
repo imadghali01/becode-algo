2643a04cf4fb0ddda50f3f5e43fd994ff1350efd
imadghali01 <imadghali01@gmail.com>
2024-09-19 15:25:07 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,54 +1,21 @@
-/*Write a function called ransomNote which takes two parameters : noteText and magazineText.
+/*A palindrome is any word or phrase that spells the same way both backward and forward.
 
-The goal is : with all the words of magazineText, you should be able to create the noteText.
+For example : race car is a palindrome. (don't take spaces, periods, dots, etc in consideration)
 
-The function should return true if we are able to write the noteText with the words from magazineText or false if we can't.
+Other examples : Madam, I'm Adam, radar, kayak, etc.
+
+You should create a function isPalindrome that takes a parameter text and returns a boolean indicating if the provided text is a palindrome or not.
 Notes
 
-Here are two examples :
+    You should be able to to ignore all special characters, spaces, dots, periods, etc. and be case insensitive
 
-noteText = "this is a secret note to you from a secret admirer"
+Expected results
 
-magazineText = "puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"
+isPalindrome("kayak") ---> true
+isPalindrome("race car") ---> true
+isPalindrome("hello world") ---> false
+isPalindrome("Madam, I'm Adam") ---> true*/
 
-In the case above, it would be false. Why ? Because all the words are present, but secret appears only once in the magazineText, but there are two secret in the noteText
-
-If the noteText was
-
-"this is a note to you from a secret admirer"
-
-That would work and would be true
-
-Test your code with these text and others.*/
-
-function ransomNote(noteText, magazineText){
-    let noteSplit = noteText.split(/\s+/);
-    let magazineSplit = magazineText.split(/\s+/);
+function isPalindrome(string){
     
-    function countWords(wordsArray) {
-        let wordCount = {};
-        for (let word of wordsArray) {
-            wordCount[word] = (wordCount[word] || 0) + 1;
-        }
-        return wordCount;
-    }
-
-    // Compter les occurrences des mots dans noteText et magazineText
-    let noteWordCount = countWords(noteSplit);
-    let magazineWordCount = countWords(magazineSplit);
-
-    // Vérifier si chaque mot de noteText est présent en quantité suffisante dans magazineText
-    for (let word in noteWordCount) {
-        if (!magazineWordCount[word] || magazineWordCount[word] < noteWordCount[word]) {
-            return false;
-        }
-    }
-
-    return true;
 }
-
-console.log(ransomNote("this is a secret note to you from a secret admirer", 
-"puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"));
-console.log(ransomNote("this is a note to you from a secret admirer", 
-"puerto rico is a great place you must hike far from town to find a secret waterfall that i am an admirer of but note that it is not as hard as it seems this is my advice to you"));
-    
