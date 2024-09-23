cfdd64ac45e2711584987b741c211369eb266a2c
imadghali01 <imadghali01@gmail.com>
2024-09-23 10:24:56 +0200

    algo-optional-00

---
+++ app.js
@@ -0,0 +1,19 @@
+/*Create a function that remove duplicates from an array. (oneliner).
+
+ex:
+
+removeDuplicates([4, 9, 5, 1, 3, 2, 4, 1, 8]);
+// Result : [4, 9, 5, 1, 3, 2, 8]
+
+removeDuplicates(["hello", "world", "goodbye", "world"]);
+// Result : ['hello', 'world', 'goodbye']
+
+removeDuplicates([true, true, false, true, true, false]);
+// Result : [true, false]*/
+
+const removeDuplicates = (array)=>{return [...new Set(array)];};
+console.log(removeDuplicates([true, true, false, true, true, false]));
+console.log(removeDuplicates(["hello", "world", "goodbye", "world"]));
+console.log(removeDuplicates([4, 9, 5, 1, 3, 2, 4, 1, 8]));
+
+
